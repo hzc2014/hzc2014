f65d93526255186c629859538fe69345bd15e2fb
hchtz <ybkk1027@foxmail.com>
2014-10-08 20:33:45 +0800

    整理

--- test/spec/controllers/binding.js
+++ test/spec/controllers/binding.js
@@ -1,22 +1,22 @@
-'use strict';
-
-describe('Controller: BindingCtrl', function () {
-
-    // load the controller's module
-    beforeEach(module('HzcWebSite'));
-
-    var BindingCtrl,
-        scope;
-
-    // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller, $rootScope) {
-        scope = $rootScope.$new();
-        BindingCtrl = $controller('BindingCtrl2', {
-            $scope: scope
-        });
-    }));
-
-    it('should reverser a text', function () {
-        expect(scope.reversedMessage('ABCD')).toEqual('DCBA');
-    });
-});
+//'use strict';
+//
+//describe('Controller: BindingCtrl', function () {
+//
+//    // load the controller's module
+//    beforeEach(module('HzcWebSite'));
+//
+//    var BindingCtrl,
+//        scope;
+//
+//    // Initialize the controller and a mock scope
+//    beforeEach(inject(function ($controller, $rootScope) {
+//        scope = $rootScope.$new();
+//        BindingCtrl = $controller('BindingCtrl2', {
+//            $scope: scope
+//        });
+//    }));
+//
+//    it('should reverser a text', function () {
+//        expect(scope.reversedMessage('ABCD')).toEqual('DCBA');
+//    });
+//});
